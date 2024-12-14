279b95c93b5015634fa5d2bf0e688d49f5f30f80
Mohammed Jaradat <Msjaradat352@gmail.com>
2024-12-14 01:11:59 -0500

    added a help button for the title

---
+++ Bang.js
@@ -0,0 +1,9 @@
+function openHelp() {
+  const helpBox = document.getElementById('helpBox');
+  helpBox.style.display = 'block';
+}
+
+function closeHelp() {
+  const helpBox = document.getElementById('helpBox');
+  helpBox.style.display = 'none';
+}
